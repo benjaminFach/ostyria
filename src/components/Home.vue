d145889ffe4bdb28a75a1b46c671ae48fabf7112
benjaminFach <benjaminfach@gmail.com>
2025-09-09 13:03:35 +0000

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p>
+      Edit
+      <code>components/Home.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p class="read-the-docs">Click on the logo to learn what's tracked</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
